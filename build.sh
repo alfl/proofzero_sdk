4ef2a0bca61987d93b6750499ff1064b0b03ab49
Alexander Flanagan <alex.flanagan@gmail.com>
2021-03-09 14:38:58 -0500

    wip, add build script output and error case

--- build.sh
+++ build.sh
@@ -1,3 +1,6 @@
 #!/usr/bin/env bash
+set -e
+echo "-> Building library."
 nbdev_build_lib --fname index.ipynb --fname core.ipynb
+echo "-> Building docs."
 nbdev_build_docs --fname index.ipynb --fname core.ipynb
